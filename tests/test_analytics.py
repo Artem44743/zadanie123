41262a348cfae33121085e5b0449322461ea6b36
Artem44743 <urt666888@gmail.com>
2026-06-24 10:39:58 +0300

    test: Добавление модульных тестов для валидации логики

--- tests/test_analytics.py
+++ tests/test_analytics.py
@@ -2,5 +2,5 @@ import pytest
 import pandas as pd
 from app.services.data_manager import DataProcessor
 def test_filter():
-    df = pd.DataFrame({"Категория": ["А", "Б"], "Выручка_USD": [10, 20]})
+    df = pd.DataFrame({"Категория": ["А", "Б"], "Выручка_USD":})
     assert len(DataProcessor.filter_by_category(df, "А")) == 1
